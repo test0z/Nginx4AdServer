1a4d4913b97cd2bd97e43bb73d47a00e8bb0158a
zhuqiwen <zhuqiwen@trycatch.com.cn>
2016-10-17 10:05:26 +0800

    以pid作为adselect连接的identity

--- src/adservice/adservice/service/core/adselectv2/ad_select_client.h
+++ src/adservice/adservice/service/core/adselectv2/ad_select_client.h
@@ -6,28 +6,31 @@
 
 #include <memory>
 
+#include <unistd.h>
+
 #include <mtty/types.h>
 #include <mtty/zmq.hpp>
 
 namespace adservice {
 namespace adselectv2 {
 
-#define ADX_OTHER		0
+#define ADX_OTHER 0
 #define ADSELECT_DEFAULT_TIMEOUT 15
 
 	class AdSelectClient {
 	public:
-
-		AdSelectClient(const std::string & url, std::map<int,int>& timeoutMap)
+		AdSelectClient(const std::string & url, std::map<int, int> & timeoutMap)
 			: serverUrl_(url)
 		{
+			std::string identity = std::to_string(getpid());
+			socket_.setsockopt(ZMQ_IDENTITY, identity.c_str(), identity.length());
 			socket_.connect(url);
 			pollitems_[0] = { socket_, 0, ZMQ_POLLIN, 0 };
-			for(auto item : timeoutMap){
-				selectTimeouts_.insert(std::make_pair(item.first,std::chrono::milliseconds(item.second)));
+			for (auto item : timeoutMap) {
+				selectTimeouts_.insert(std::make_pair(item.first, std::chrono::milliseconds(item.second)));
 			}
-			if(selectTimeouts_.find(ADX_OTHER)==selectTimeouts_.end()){
-				selectTimeouts_.insert(std::make_pair(ADX_OTHER,std::chrono::milliseconds(ADSELECT_DEFAULT_TIMEOUT)));
+			if (selectTimeouts_.find(ADX_OTHER) == selectTimeouts_.end()) {
+				selectTimeouts_.insert(std::make_pair(ADX_OTHER, std::chrono::milliseconds(ADSELECT_DEFAULT_TIMEOUT)));
 			}
 		}
 
@@ -37,7 +40,7 @@ namespace adselectv2 {
 
 	private:
 		std::string serverUrl_;
-		std::map<int,std::chrono::milliseconds> selectTimeouts_;
+		std::map<int, std::chrono::milliseconds> selectTimeouts_;
 
 		zmq::context_t context_{ 1 };
 		zmq::socket_t socket_{ context_, ZMQ_DEALER };
